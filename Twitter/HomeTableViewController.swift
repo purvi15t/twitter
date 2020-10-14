7a43ee2e50a5a24479f1b392dd3d3b94855d99ac
purvi15t <pgupta1598@gmail.com>
2020-10-14 13:41:20 -0500

    Assignment 4 including favorite/unfavorite tweet & retweet.

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -18,16 +18,21 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         loadTweet()
-
         myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
-        
         tableView.refreshControl = myRefreshControl
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 150
+        print("successful")
+        
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweet()
+    }
+    
     @objc func loadTweet(){
-        
         numberOfTweet = 20
         let myURL = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myparams = ["count":numberOfTweet]
@@ -101,18 +106,22 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data{
             cell.profileImageView.image = UIImage(data: imageData)
         }
-        return cell
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
+         return cell
     }
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return tweetArray.count
     }
-  
-   
-
+    
 }
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -11,11 +11,67 @@ import UIKit
 class TweetCellTableViewCell: UITableViewCell {
 
     @IBOutlet weak var profileImageView: UIImageView!
-    
     @IBOutlet weak var userNameLabel: UILabel!
-    
     @IBOutlet weak var tweetContent: UILabel!
     
+    @IBOutlet weak var favButton: UIButton!
+    @IBOutlet weak var retweetButton: UIButton!
+    
+    var favorited:Bool = false
+    var tweetId:Int =  -1
+    var retweeted:Bool = false
+    
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        
+        let tobeFavorited = !favorited
+        if(tobeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (error) in
+                print("Favorite didn't succeed \(error)")
+            })
+        }
+        else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (error) in
+                print("unfavorite didn't succeed \(error)")
+            })
+        }
+        
+    }
+    
+    @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("error in retweeting \(error)")
+        })
+    }
+    
+   
+    func setFavorite(_ isFavorited:Bool){
+        favorited = isFavorited
+        if(favorited){
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        }
+        else{
+            favButton.setImage(UIImage(named: "favor-icon-1"), for: UIControl.State.normal)
+        }
+    }
+    
+    func setRetweeted(_ isRetweeted:Bool){
+        
+        if(isRetweeted){
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }
+        else{
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
